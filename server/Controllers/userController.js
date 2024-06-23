9d5453923651163c03ac36e1e63cbbb64ac3d3ec
aakashstha1 <aakash.cerestha90@gmail.com>
2024-06-23 15:28:11 +0545

    Day23

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -58,4 +58,50 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(400).json("Invalid email or password...");
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword)
+      return res.status(400).json("Invalid email or password...");
+
+    const token = createToken(user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email,
+      token,
+    });
+  } catch (error) {
+    console.error("Error Loging user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const findUser = async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const user = await userModel.findById(userId);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error Finding user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error Finding users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = { registerUser, loginUser, findUser, getUsers };
